7285d870ec39c5bc13572a72b263fe30c2611126
Adil <99889589+Ad1l9@users.noreply.github.com>
2023-10-10 01:09:13 +0400

    Add project files.

---
+++ StringMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace StringMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = "        EverytHing  i neeD,    is a CuP   of       CoFFee    ";
+
+            Console.WriteLine(StringFormat(inputText));
+        }
+
+        public static string StringFormat(string input)
+        {
+            input=input.Trim().ToUpper();
+            string[] text = input.Split(' ');
+
+            StringBuilder result=new StringBuilder();
+
+
+            foreach (var item in text)
+            {
+                if (!string.IsNullOrEmpty(item))
+                {
+                    string newItem = item[0] + item.Substring(1).ToLower();
+                    result.Append(newItem+' ');
+                }
+            }
+            return result.ToString();
+        }
+
+    }
+}
